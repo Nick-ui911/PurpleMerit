83e9606be7f3594e3d47c842511f95ebaf04e5b7
Nick-ui911 <nbaghel392@gmail.com>
2025-12-30 02:00:15 +0530

    same issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,16 +7,15 @@ import adminRoutes from "./routes/adminRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://purple-merit-rho.vercel.app"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://purple-merit-rho.vercel.app"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
