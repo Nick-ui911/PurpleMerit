b54cee2ac03219d7e8b9809b8af3528c745ce609
Nick-ui911 <nbaghel392@gmail.com>
2025-12-30 02:06:51 +0530

    allowed preflight

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,6 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export const authMiddleware = (req, res, next) => {
+    // ✅ Allow preflight requests
+  if (req.method === "OPTIONS") {
+    return next();
+  }
   const token = req.cookies.token;
 
   if (!token) {
